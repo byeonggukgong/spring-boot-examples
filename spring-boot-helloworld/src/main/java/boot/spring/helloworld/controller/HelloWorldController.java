897644e324080c262acb192159ce2179b1a8de62
Byeongguk Gong <byeonggukgong@gmail.com>
2019-01-26 17:45:36 +0900

    Update Spring Boot Helloworld

---
+++ spring-boot-helloworld/src/main/java/boot/spring/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package boot.spring.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String helloworld() {
+        return "Hello, world!";
+    }
+
+}
